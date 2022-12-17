197dc53223e812ab731bb16bbeec8c7175aaacdb
nakagawa <Takuya.Nakagawa@kusurinomadoguchi.co.jp>
2022-12-17 10:40:03 +0900

    フォーム作成まで

--- next-market/pages/api/item/create.js
+++ next-market/pages/api/item/create.js
@@ -1,11 +1,18 @@
 // pages\api\item\create.js
 
 import connectDB from "../../../utils/database";
+import { ItemModel } from "../../../utils/schemaModels";
 
-const createItem = (req, res) => {
-    connectDB();
-    console.log(req.body.title);
-    return res.status(200).json({ message: "アイテム作成" });
+const createItem =  async(req, res) => {
+    try {
+        await connectDB();
+        console.log(req.body.title);
+        await ItemModel.create(req.body)
+        return res.status(200).json({ message: "アイテム作成成功" });
+    } catch (err) {
+        return res.status(400).json({ message: "アイテム作成失敗" });
+     }
+
 };
 
 export default createItem;
